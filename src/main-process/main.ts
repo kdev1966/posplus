3ac29647e08b380e6750c7ec27f676ee5ceb2e53
kdev1966 <kaabaoui.othman@gmail.com>
2025-11-16 21:30:28 +0100

    Modif main.ts

--- src/main-process/main.ts
+++ src/main-process/main.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow } from 'electron'
+import electron from 'electron'
+const { app, BrowserWindow } = electron
 import path from 'path'
 import log from 'electron-log'
 import { db } from './services/database/db'
@@ -7,7 +8,7 @@ import { db } from './services/database/db'
 log.transports.file.level = 'info'
 log.transports.console.level = 'debug'
 
-let mainWindow: BrowserWindow | null = null
+let mainWindow: InstanceType<typeof BrowserWindow> | null = null
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 
